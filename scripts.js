1d8f2882465cac83ef45d312b9a38b22ea8cd08f
eugene <info@eugeneyeboah.com>
2022-02-23 22:29:25 -0500

    save

--- scripts.js
+++ scripts.js
@@ -10,23 +10,36 @@ function passwordGenerator() {
   const numbersBoolean = document.getElementById('numbers').checked;
   const symbolsBoolean = document.getElementById('symbols').checked;
 
+  var resultArray = []
+  var num = 0;
+  while (resultArray.length < passwordLength) {
+    resultArray.push(num);
+    num++;
+  }
+
   console.log(passwordLength);
   console.log(upperCaseBoolean);
   console.log(lowerCaseBoolean);
   console.log(numbersBoolean);
   console.log(symbolsBoolean);
+
+  document.getElementById('result').innerText = resultArray.join('');
 }
 
 // Generattor functions
 
 function getLowerCaseLetter() {
+  console.log('getLowerCaseLetter');
 }
 
 function getUpperCaseLetter() {
+  console.log('getUpperCaseLetter');
 }
 
 function getNumber() {
+  console.log('getNumber');
 }
 
 function getSymbol() {
+  console.log('getSymbol');
 }
