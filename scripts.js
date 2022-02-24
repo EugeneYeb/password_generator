8678a145becbc225eedb5b29109965a9f54ae042
eugene <info@eugeneyeboah.com>
2022-02-23 23:47:47 -0500

    save - fixing error when trying to call functions in array

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
 const generatePasswordButton = document.getElementById(`generate`);
-
 generatePasswordButton.addEventListener("click", passwordGenerator);
 
 // Generator Main Functions
@@ -10,36 +9,69 @@ function passwordGenerator() {
   const numbersBoolean = document.getElementById('numbers').checked;
   const symbolsBoolean = document.getElementById('symbols').checked;
 
-  var resultArray = []
-  var num = 0;
+  var selectedFunctions = methodRandomizer(upperCaseBoolean, lowerCaseBoolean, numbersBoolean, symbolsBoolean);
+  console.log(selectedFunctions);
+  var resultArray = [];
+
   while (resultArray.length < passwordLength) {
-    resultArray.push(num);
-    num++;
+
+    var functionNumber = randomNumber(selectedFunctions.length, 0);
+
+    resultArray.push(selectedFunctions[functionNumber]);
+    // resultArray.push(String.fromCharCode(97));
+    // console.log(passwordLength);
+    // console.log(upperCaseBoolean);
+    // console.log(lowerCaseBoolean);
+    // console.log(numbersBoolean);
+    // console.log(symbolsBoolean);
   }
-
-  console.log(passwordLength);
-  console.log(upperCaseBoolean);
-  console.log(lowerCaseBoolean);
-  console.log(numbersBoolean);
-  console.log(symbolsBoolean);
-
   document.getElementById('result').innerText = resultArray.join('');
 }
 
-// Generattor functions
+//Method Randomizers
+function methodRandomizer(uppercase, lowercase, numbers, symbols) {
+  var methodArrays = [];
 
-function getLowerCaseLetter() {
-  console.log('getLowerCaseLetter');
+  if (uppercase) {
+    methodArrays.push(getUpperCaseLetter);
+  }
+
+  if (lowercase) {
+    methodArrays.push(getLowerCaseLetter);
+  }
+
+  if (numbers) {
+    methodArrays.push(getNumber);
+  }
+
+  if (symbols) {
+    methodArrays.push(getSymbol);
+  }
+  return methodArrays;
 }
 
+// Generator functions
+
 function getUpperCaseLetter() {
-  console.log('getUpperCaseLetter');
+  // console.log('getUpperCaseLetter');
+
+  String.fromCharCode(randomNumber(91, 65));
+}
+function getLowerCaseLetter() {
+  // console.log('getLowerCaseLetter');
+  String.fromCharCode(randomNumber(123, 97));
 }
 
 function getNumber() {
-  console.log('getNumber');
+  // console.log('getNumber');
+  String.fromCharCode(randomNumber(58, 0));
 }
 
 function getSymbol() {
-  console.log('getSymbol');
+  // console.log('getSymbol');
+  String.fromCharCode(randomNumber(48, 0));
+}
+
+function randomNumber(max, min) {
+  return Math.floor(Math.random() * (max - min) + min);
 }
