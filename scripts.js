eda9984665772bb1eaf5e8ded425b8e455b67c5a
eugene <info@eugeneyeboah.com>
2022-02-24 00:03:14 -0500

    save - need to work on clipboard functionality

--- scripts.js
+++ scripts.js
@@ -17,13 +17,8 @@ function passwordGenerator() {
 
     var functionNumber = randomNumber(selectedFunctions.length, 0);
 
-    resultArray.push(selectedFunctions[functionNumber]);
-    // resultArray.push(String.fromCharCode(97));
-    // console.log(passwordLength);
-    // console.log(upperCaseBoolean);
-    // console.log(lowerCaseBoolean);
-    // console.log(numbersBoolean);
-    // console.log(symbolsBoolean);
+    resultArray.push(selectedFunctions[functionNumber]());
+    // console.log()
   }
   document.getElementById('result').innerText = resultArray.join('');
 }
@@ -47,29 +42,26 @@ function methodRandomizer(uppercase, lowercase, numbers, symbols) {
   if (symbols) {
     methodArrays.push(getSymbol);
   }
+
   return methodArrays;
+
 }
 
 // Generator functions
-
 function getUpperCaseLetter() {
-  // console.log('getUpperCaseLetter');
-
-  String.fromCharCode(randomNumber(91, 65));
+  return String.fromCharCode(randomNumber(91, 65));
 }
+
 function getLowerCaseLetter() {
-  // console.log('getLowerCaseLetter');
-  String.fromCharCode(randomNumber(123, 97));
+  return String.fromCharCode(randomNumber(123, 97));
 }
 
 function getNumber() {
-  // console.log('getNumber');
-  String.fromCharCode(randomNumber(58, 0));
+  return String.fromCharCode(randomNumber(58, 48));
 }
 
 function getSymbol() {
-  // console.log('getSymbol');
-  String.fromCharCode(randomNumber(48, 0));
+  return String.fromCharCode(randomNumber(48, 33));
 }
 
 function randomNumber(max, min) {
